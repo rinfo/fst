65eda5e9a5d52e2f955e1596f6a24ecf952c71f2
kamidev <jonas.beckman@gmail.com>
2011-03-23 08:15:13 +0100

    Remove deprecation warnings when running Django 1.3

--- fst_web/settings.py
+++ fst_web/settings.py
@@ -3,12 +3,12 @@ import os
 
 # Django settings for lagrumsapp project.
 
-DATABASE_ENGINE = 'sqlite3'     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(os.path.dirname(__file__), 'database/lagrumsapp.db').replace('\\','/') #Path to sqlite 3 db-file
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES_ENGINE = 'sqlite3'     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASES_NAME = os.path.join(os.path.dirname(__file__), 'database/lagrumsapp.db').replace('\\','/') #Path to sqlite 3 db-file
+DATABASES_USER = ''             # Not used with sqlite3.
+DATABASES_PASSWORD = ''         # Not used with sqlite3.
+DATABASES_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASES_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
