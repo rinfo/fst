2fa974f217549950a7d1a05619b79b57cbdbad10
kamidev <jonas.beckman@gmail.com>
2011-03-28 11:59:37 +0200

    Update settings for Django 1.3

--- fst_web/settings.py
+++ fst_web/settings.py
@@ -3,12 +3,12 @@ import os
 
 # Django settings for lagrumsapp project.
 
-DATABASES_ENGINE = 'sqlite3'     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASES_NAME = os.path.join(os.path.dirname(__file__), 'database/lagrumsapp.db').replace('\\','/') #Path to sqlite 3 db-file
-DATABASES_USER = ''             # Not used with sqlite3.
-DATABASES_PASSWORD = ''         # Not used with sqlite3.
-DATABASES_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASES_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'database/lagrumsapp.db').replace('\\','/') #Path to sqlite 3 db-file
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -83,8 +83,9 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.admin',
-    'fst_web.fs_doc',
+    'fst_web.fs_doc'
 )
 
 try:
