8804fb34a659ac3a5b41b042f97474206803c38f
kamidev <jonas.beckman@gmail.com>
2011-05-01 12:25:50 +0200

    Remove unnecessary app

--- fst_web/fs_doc/tests.py
+++ fst_web/fs_doc/tests.py
@@ -7,106 +7,123 @@ import os
 import shutil
 
 class RinfoTestCase(TestCase):
-    # Ladda exempeldata från rinfo/fixtures/exempeldata.json innan test startar
+    # Initialize with sample data from fsdoc/fixtures/exempeldata.json
     fixtures = ['exempeldata.json']
 
     def setUp(self):
         base = os.path.join(os.path.dirname(__file__))
         testdocs = os.path.join(base, "../dokument_test")
 
-        # Skapa mapp för testdokument
+        # Create temporary folder for testing documents
         if not os.path.exists(testdocs):
             os.mkdir(testdocs)
 
-        # Kopiera filer från fixtures till dokument_test
-        shutil.copy(os.path.join(base, "fixtures/EXFS-2009-1.pdf"), testdocs)
-        shutil.copy(os.path.join(base, "fixtures/EXFS-2009-1-bilaga.pdf"), testdocs)
-        shutil.copy(os.path.join(base, "fixtures/EXFS-2009-2.pdf"), testdocs)
-        shutil.copy(os.path.join(base, "fixtures/EXFS-2009-3.pdf"), testdocs)
+        # Move documents from fixtures to temporary test folder
+        shutil.copy(os.path.join(base, 
+                                 "fixtures/foreskrift/EXFS-2009-1.pdf"), testdocs)
+        shutil.copy(os.path.join(base, 
+                                 "fixtures/bilaga/EXFS-2009-1-bilaga.pdf"), testdocs)
+        shutil.copy(os.path.join(base, 
+                                 "fixtures/foreskrift/EXFS-2009-2.pdf"), testdocs)
+        shutil.copy(os.path.join(base, 
+                                 "fixtures/foreskrift/EXFS-2009-3.pdf"), testdocs)
 
     def tearDown(self):
         base = os.path.join(os.path.dirname(__file__))
         testdocs = os.path.join(base, "../dokument_test")
 
-        # Släng testfiler
+        # Delete temporary testfolder
         shutil.rmtree(testdocs)
 
-    # Verifiera att startsidan kan läsas och att den visar de tre
-    # exempelföreskrifterna
+        
+    # Verify that start page loads and displays correct sample documents
     def test_startsida(self):
         response = self.client.get('/')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertContains(response, "EXFS 2009:1 Föreskrift om administration hos statliga myndigheter")
-        self.assertContains(response, "EXFS 2009:2 Föreskrift om ändring i föreskrift 2009:1 om administration hos statliga myndigheter")
-        self.assertContains(response, "EXFS 2009:3 Föreskrift om budgetering hos statliga myndigheter")
+        self.assertContains(response, 
+                            "EXFS 2009:1 Föreskrift om administration hos statliga myndigheter")
+        self.assertContains(response, 
+                            "EXFS 2009:2 Föreskrift om ändring i föreskrift 2009:1 om administration hos statliga myndigheter")
+        self.assertContains(response, 
+                            "EXFS 2009:3 Föreskrift om budgetering hos statliga myndigheter")
 
-
-    # Verifiera att sidan för en enskild föreskrift visas
+    # Verify that detail page for document 'Myndighetsforeskrift' loads
+    # with correct sample data
     def test_foreskrift(self):
         response = self.client.get('/publ/EXFS/2009:1/')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertContains(response, "<h1>EXFS 2009:1 Föreskrift om administration hos statliga myndigheter")
+        self.assertContains(response, 
+                            "<h1>EXFS 2009:1 Föreskrift om administration hos statliga myndigheter")
 
-
-    # Verifiera att publicerad föreskrift har metadata i RDF-format
+    # Verify that published 'Myndighetsforeskrift' document has 
+    # correct RDF metadata' for keywords (Django class'Amnesord')
     def test_rdfdata(self):
         response = self.client.get('/publ/EXFS/2009:1/rdf')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertEqual(response['content-type'], 'application/rdf+xml; charset=utf-8')
-        self.assertContains(response, "Föreskrift om administration hos statliga myndigheter")
-        self.assertContains(response, "<dces:subject xml:lang=\"sv\">Administration</dces:subject>")
+        self.assertEqual(response['content-type'], 
+                         'application/rdf+xml; charset=utf-8')
+        self.assertContains(response, 
+                            "Föreskrift om administration hos statliga myndigheter")
+        self.assertContains(response, 
+                            "<dces:subject xml:lang=\"sv\">Administration</dces:subject>")
 
-
-    # Verifiera att publicerad föreskrift har information om relation till EG-direktiv
+    # Verify that published 'Myndighetsforeskrift' document has 
+    # correct RDF metadata for legal directives (Django class 'CelexReferens')
     def test_egdirektiv(self):
         response = self.client.get('/publ/EXFS/2009:1/rdf')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertEqual(response['content-type'], 'application/rdf+xml; charset=utf-8')
-        self.assertContains(response, "<genomforDirektiv rdf:resource=\"http://rinfo.lagrummet.se/ext/eur-lex/31979L0409\"/>")
+        self.assertEqual(response['content-type'], 
+                         'application/rdf+xml; charset=utf-8')
+        self.assertContains(response, 
+                            "<genomforDirektiv rdf:resource=\"http://rinfo.lagrummet.se/ext/eur-lex/31979L0409\"/>")
 
-    # Verifiera att publicerad föreskrift 2009:2 har information om omtryck
+    # Verify that published 'Myndighetsforeskrift' document has 
+    # correct RDF metadata for property 'omtryck'
     def test_omtryck(self):
         response = self.client.get('/publ/EXFS/2009:2/rdf')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertEqual(response['content-type'], 'application/rdf+xml; charset=utf-8')
+        self.assertEqual(response['content-type'],
+                         'application/rdf+xml; charset=utf-8')
         self.assertContains(response, "<omtryckAv rdf:resource=\"http://rinfo.lagrummet.se/publ/exfs/2009:1\"/>")
 
-        # och att omtrycksinformation saknas om det inte är ett omtryck
+        # No metadata should be genereated unless property 'omtryck' is true
         response = self.client.get('/publ/EXFS/2009:3/rdf')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertEqual(response['content-type'], 'application/rdf+xml; charset=utf-8')
+        self.assertEqual(response['content-type'], 
+                         'application/rdf+xml; charset=utf-8')
         self.assertNotContains(response, "<omtryckAv")
 
-
-    # Verifiera att feed kan läsas och att den innehåller tre <entry>-poster
+    # Verify that an empty Atom feed is created and can be read
+    # NOTE - Atom feed is empty until entries are explicitly published
     def test_atomfeed(self):
         response = self.client.get('/feed/')
         self.failUnlessEqual(response.status_code, 200)
-        self.assertEqual(response['content-type'], 'application/atom+xml; charset=utf-8')
-        self.assertContains(response, "<entry>", 3)
+        self.assertEqual(response['content-type'], 
+                         'application/atom+xml; charset=utf-8')
+        # Make
+        self.assertContains(response, "<entry>", 0)
 
-
-    # Verifiera att checksumma för rdf-metadata angiven i atomentry är korrekt
+    # Verify that checksum of document in Atom feed is correct
+    # NOTE - Atom feed is empty until entries are explicitly published
     def test_rdfmd5(self):
         NS_ATOM="http://www.w3.org/2005/Atom"
         NS_ATOMLE="http://purl.org/atompub/link-extensions/1.0"
-
-        # Hämta atom-entry för dokument
+        # Get Atom feed
         response = self.client.get('/feed/')
         self.failUnlessEqual(response.status_code, 200)
 
-        # Hämta md5-summa för entry för föreskrift 2009:1
-        dom=parseString(response.content)
-        avlast_md5=""
-        
-        for link in dom.getElementsByTagNameNS(NS_ATOM, 'link'):
+        # Get links to documents in Atom feed
+        dom = parseString(response.content)
+        links = dom.getElementsByTagNameNS(NS_ATOM, 'link')
+        avlast_md5 = ""           
+        for link in links:
+            # Look for specific sample document in feed
             if link.getAttribute("href")=="/publ/EXFS/2009:1/rdf":
-                avlast_md5=link.getAttributeNS(NS_ATOMLE, "md5")
-                
-
-        # Jämför avläst md5summa med egenberäknad
-        response = self.client.get('/publ/EXFS/2009:1/rdf')
-        md5=hashlib.md5()
-        md5.update(response.content)
-        beraknad_rdfmd5=md5.hexdigest()
-        self.assertEqual(beraknad_rdfmd5, avlast_md5)
+                # Read document checksum from feed
+                avlast_md5 = link.getAttributeNS(NS_ATOMLE, "md5")
+                # Compare checksum from feed with checksum of current document
+                response = self.client.get('/publ/EXFS/2009:1/rdf')
+                md5=hashlib.md5()
+                md5.update(response.content)
+                beraknad_rdfmd5=md5.hexdigest()
+                self.assertEqual(beraknad_rdfmd5, avlast_md5)
