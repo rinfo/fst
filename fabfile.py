9c36417af3364b77853b62d9a924d5693e44dce0
kamidev <jonas.beckman@gmail.com>
2011-05-06 18:14:11 +0200

    Remove noob fab method :-)

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,5 @@
 # -*- coding: UTF-8 -*-
 from fabric.api import *
-
-#def dev()
-#def prod()
-
-#def setup_host()
-
-def hello():
-    print("Hello world!")
     
 def clean_db():
     local("rm -rf database/fst_demo.db;python manage.py syncdb --noinput;python manage.py loaddata fs_doc/fixtures/exempeldata.json")
