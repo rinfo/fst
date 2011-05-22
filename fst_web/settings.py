e55b26c06f2afb9f458032692e951b1ee9d7542b
kamidev <jonas.beckman@gmail.com>
2011-05-22 15:52:36 +0200

    Change settings to allow cconditional GET operations

--- fst_web/settings.py
+++ fst_web/settings.py
@@ -103,6 +103,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
