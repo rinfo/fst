23540359422501ebd8a1b833c426cdfb1a3dfe00
kamidev <jonas.beckman@gmail.com>
2011-05-27 13:59:50 +0200

    Add new fixture to fabric script

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,21 @@
 from fabric.api import *
 
 def move_sampledocs_to_fixture():
+    local("mkdir -p uploads/foreskrift")
+    local("rm -f uploads/foreskrift/*.pdf")
     local("cp fs_doc/fixtures/foreskrift/*.pdf uploads/foreskrift/")
+    
+    local("mkdir -p uploads/bilaga")
+    local("rm -f uploads/bilaga/*.pdf")
     local("cp fs_doc/fixtures/bilaga/*.pdf uploads/bilaga/")
+    
+    local("mkdir -p uploads/allmanna_rad")
+    local("rm -f uploads/allmanna_rad/*.pdf")
     local("cp fs_doc/fixtures/allmanna_rad/*.pdf uploads/allmanna_rad/")
+    
+    local("mkdir -p uploads/konsoliderad_foreskrift")
+    local("rm -f uploads/konsoliderad_foreskrift/*.pdf")
+    local("cp fs_doc/fixtures/konsoliderad_foreskrift/*.pdf uploads/konsoliderad_foreskrift/")
     
 def clean_db():
     local("rm -rf database/fst_demo.db;python manage.py syncdb --noinput;python manage.py loaddata fs_doc/fixtures/exempeldata.json")
