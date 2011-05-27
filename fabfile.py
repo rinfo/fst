645fb0f9a0f89152a13b098f240710b2628b32b1
Niklas Lindström <lindstream@gmail.com>
2011-05-27 16:19:35 +0200

    Renamed clean to reset in fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,37 @@
 # -*- coding: UTF-8 -*-
 from fabric.api import *
 
+def reset():
+    move_sampledocs_to_fixture()
+    reset_db()
+
 def move_sampledocs_to_fixture():
     local("mkdir -p uploads/foreskrift")
     local("rm -f uploads/foreskrift/*.pdf")
     local("cp fs_doc/fixtures/foreskrift/*.pdf uploads/foreskrift/")
-    
+
     local("mkdir -p uploads/bilaga")
     local("rm -f uploads/bilaga/*.pdf")
     local("cp fs_doc/fixtures/bilaga/*.pdf uploads/bilaga/")
-    
+
     local("mkdir -p uploads/allmanna_rad")
     local("rm -f uploads/allmanna_rad/*.pdf")
     local("cp fs_doc/fixtures/allmanna_rad/*.pdf uploads/allmanna_rad/")
-    
+
     local("mkdir -p uploads/konsoliderad_foreskrift")
     local("rm -f uploads/konsoliderad_foreskrift/*.pdf")
-    local("cp fs_doc/fixtures/konsoliderad_foreskrift/*.pdf uploads/konsoliderad_foreskrift/")
-    
-def clean_db():
-    local("rm -rf database/fst_demo.db;python manage.py syncdb --noinput;python manage.py loaddata fs_doc/fixtures/exempeldata.json")
+    local("cp fs_doc/fixtures/konsoliderad_foreskrift/*.pdf "
+          "uploads/konsoliderad_foreskrift/")
+
+def reset_db():
+    local("rm -rf database/fst_demo.db && "
+          "python manage.py syncdb --noinput && "
+          "python manage.py loaddata fs_doc/fixtures/exempeldata.json")
+
+def reset_test():
+    reset()
+    test()
 
 def test():
     local("python manage.py test")
-    
-def clean_test():
-    move_sampledocs_to_fixture()
-    clean_db()
-    test()
-
 
