9baeabe8348e54bf35587ed614eef39f18b08ce9
kamidev <jonas.beckman@gmail.com>
2011-06-17 22:41:18 +0200

    Make loaded DB look better in admin: add 'Myndighet and 'Forfattningssamling'

--- tools/load_db.py
+++ tools/load_db.py
@@ -10,15 +10,15 @@ def get_insert_string(row):
 	for key, val in row.items():
 		fields = fields + key + ", "
 		values = values + "'" + val + "'" + ", "
-		
-	fields = fields[:-2] + ")"	
+	
+	fields = fields[:-2] + ")"
 	values = values[:-2] + ")"
 	return fields + values
 
 def fill_table(table_name, data, db_connection):
-	for row in data[table_name]:	
-		fill_record(table_name, row, db_connection) 
-		
+	for row in data[table_name]:
+		fill_record(table_name, row, db_connection)
+
 def fill_record(table_name, row, db_connection):
 	cursor = db_connection.cursor()
 	insert_statement = "INSERT INTO %s "  % table_name
@@ -36,7 +36,7 @@ def main():
 	
 	try:
 		# Hardcoded sample data below - replace with data from '/tools/import.py'
-		data = {'fs_doc_fsdokument': 
+		data = {'fs_doc_fsdokument':
 				[
 					{'id': '1', # id for 'FFFS 2011:1' created when loading docs
 		            'arsutgava': '2011',
@@ -44,13 +44,13 @@ def main():
 					 'is_published': '0', # False (initally nothing is published)
 		             'titel': u'Föreskrifter om ersättningssystem i kreditinstitut, värdepappersbolag och fondbolag med tillstånd för diskretionär portföljförvaltning',
 		             'sammanfattning': u'Finansinspektionen har beslutat ... utan av FFFS 2011:2',
-		             'forfattningssamling_id': '1', # ID handled by import 
+		             'forfattningssamling_id': '1', # ID handled by import
 					 'beslutsdatum': '2011-02-17', # Use string not datetime
 		             'ikrafttradandedatum': '2011-03-01',
 		             'utkom_fran_tryck': '2011-02-24',
 					  'omtryck': '0', # Sqlite uses string '0' or '1': not Python boolean False
 					  'content_md5': '' # Must be calculated somewhere
-					} 
+					}
 				],
 				'fs_doc_allmannarad':
 				[
@@ -58,13 +58,29 @@ def main():
 		             'content': u'/allmannarad/fffs2011.pdf',
 		             'beslutad_av_id': '',  # Always empty (we don't handle this yet)
 					 'utgivare_id': ''  # Always empty (we don't handle this yet)
-					}  
+					}
+				],
+				'fs_doc_Myndighet':
+				[
+					{'id': '1', # Default. Sometimes there is only one.
+		             'namn': u'Finansinspektionen',
+					}
+				],
+				'fs_doc_Forfattningssamling':
+				[
+					{'id': '1', # Default. Sometimes there is only one.
+		             'titel': u'Finansinspektionens författningssamling',
+		             'kortnamn': u'FFFS',
+					'slug': u'fffs'
+					}
 				]
 		}
 		
 		fill_table('fs_doc_fsdokument', data, db_connection)
 		fill_table('fs_doc_allmannarad', data, db_connection)
-			
+		fill_table('fs_doc_Myndighet', data, db_connection)
+		fill_table('fs_doc_Forfattningssamling', data, db_connection)
+	
 	except:
 		exc_type, exc_value, exc_traceback = sys.exc_info()
 		print exc_type, exc_value
