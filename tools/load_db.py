13a443565d41f47f8c2dd6addf4eb4ee9b3a81b6
kamidev <jonas.beckman@gmail.com>
2011-06-17 22:17:33 +0200

    First version of generic db loader

--- tools/load_db.py
+++ tools/load_db.py
@@ -3,75 +3,68 @@ import sys
 import sqlite3
 import traceback
 
-def questionsmarks(count):
-	if count == 1:
-		return "(?)"
-	else:
-		return "(?," + (count -2) * "?," + "?)"
+def get_insert_string(row):
+	fields = "("
+	values = " VALUES("
 	
-def fill_record(conn,cursor, table_name, fieldnames, values):
-	insert_sql1 = "INSERT INTO %s VALUES "  % table_name
-	insert_sql2 = questionsmarks(len(fieldnames))
-	sql = insert_sql1 + insert_sql2
-	cursor.execute(sql, values)
-	conn.commit()
+	for key, val in row.items():
+		fields = fields + key + ", "
+		values = values + "'" + val + "'" + ", "
+		
+	fields = fields[:-2] + ")"	
+	values = values[:-2] + ")"
+	return fields + values
 
-def extract_fieldnames(doc):
-	fieldnames = []
-	for tup in doc:
-		fieldnames.append(tup[0])
-	return fieldnames
-	
-def extract_values(doc):
-	values = []
-	for tup in doc:
-		values.append(tup[1])
-	return values
+def fill_table(table_name, data, db_connection):
+	for row in data[table_name]:	
+		fill_record(table_name, row, db_connection) 
+		
+def fill_record(table_name, row, db_connection):
+	cursor = db_connection.cursor()
+	insert_statement = "INSERT INTO %s "  % table_name
+	insert_data = get_insert_string(row)
+	sql = insert_statement + insert_data
+	print sql
+	cursor.execute(sql)
+	db_connection.commit()
 
 def main():
+	
 	db_path = "fst_no_docs.db"
-	conn = sqlite3.connect(db_path)
-	conn.text_factory = str  #bugger 8-bit bytestrings
-	cursor = conn.cursor()
+	db_connection = sqlite3.connect(db_path)
+	db_connection.text_factory = str  #bugger 8-bit bytestrings
 	
 	try:
-		#docs = parse_fsdocs("scraped_docs.data")
-		data = {'FSDokument':[
-		            [('id','1'), ##'FFFS 2011:1' Created by import
-		             ('arsutgava', '2011'),
-		             ('lopnummer', '1'),
-					 ('is_published', '0'), # Always null (nothing is published)
-		             ('titel', 'Föreskrifter om ersättningssystem i kreditinstitut, värdepappersbolag och fondbolag med tillstånd för diskretionär portföljförvaltning'),
-		             ('sammanfattning', 'Finansinspektionen har beslutat ... utan av FFFS 2011:2'),
-		             ('forfattningssamling', '1'), # ID handled by import 
-					 ('beslutsdatum', '2011-02-17'), # Use string not datetime
-		             ('ikrafttradandedatum', '2011-03-01'),
-		             ('utkom_fran_tryck', '2011-02-24'),
-					 ('omtryck', '0'), # Sqlite uses string '0' or '1', not Python boolean False
-					 ('content_md5', '') # Always empty (unless we calculate this before loading)
-					]
+		# Hardcoded sample data below - replace with data from '/tools/import.py'
+		data = {'fs_doc_fsdokument': 
+				[
+					{'id': '1', # id for 'FFFS 2011:1' created when loading docs
+		            'arsutgava': '2011',
+		             'lopnummer': '1',
+					 'is_published': '0', # False (initally nothing is published)
+		             'titel': u'Föreskrifter om ersättningssystem i kreditinstitut, värdepappersbolag och fondbolag med tillstånd för diskretionär portföljförvaltning',
+		             'sammanfattning': u'Finansinspektionen har beslutat ... utan av FFFS 2011:2',
+		             'forfattningssamling_id': '1', # ID handled by import 
+					 'beslutsdatum': '2011-02-17', # Use string not datetime
+		             'ikrafttradandedatum': '2011-03-01',
+		             'utkom_fran_tryck': '2011-02-24',
+					  'omtryck': '0', # Sqlite uses string '0' or '1': not Python boolean False
+					  'content_md5': '' # Must be calculated somewhere
+					} 
 				],
-				'AllmannaRad':[
-					[('fsdokument_ptr_id', '1'), #'FFFS 2011:1' Created by import
-		             ('content', '/allmannarad/fffs2011.pdf'),
-		             ('beslutad_av', ''), 
-					 ('utgivar_id', ''),
-					]
+				'fs_doc_allmannarad':
+				[
+					{'fsdokument_ptr_id': '1', # id for 'FFFS 2011:1' created when loading docs
+		             'content': u'/allmannarad/fffs2011.pdf',
+		             'beslutad_av_id': '',  # Always empty (we don't handle this yet)
+					 'utgivare_id': ''  # Always empty (we don't handle this yet)
+					}  
 				]
 		}
 		
-		for doc in data['FSDokument']:	
-			fieldnames = extract_fieldnames(doc)
-			values = extract_values(doc)					
-			fill_record(conn, cursor, "fs_doc_fsdokument",fieldnames, values)
-		
-		for doc in data['AllmannaRad']:	
-			fieldnames = extract_fieldnames(doc)
-			print fieldnames
-			values = extract_values(doc)
-			print values					
-			fill_record(conn, cursor, "fs_doc_allmannarad",fieldnames, values)
-		
+		fill_table('fs_doc_fsdokument', data, db_connection)
+		fill_table('fs_doc_allmannarad', data, db_connection)
+			
 	except:
 		exc_type, exc_value, exc_traceback = sys.exc_info()
 		print exc_type, exc_value
