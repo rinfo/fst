570096080fa54bdd989adc67a8672c3516b11254
kamidev <jonas.beckman@gmail.com>
2011-06-18 18:56:40 +0200

    Add tests to load_db.py: multiple docs of different types

--- tools/load_db.py
+++ tools/load_db.py
@@ -57,47 +57,69 @@ def main():
     try:
         # Hardcoded data below - replace with data from '/tools/import.py'
         data = {'fs_doc_fsdokument':
-        [
-            {'id': '1',  # id is created when loading docs
-             'arsutgava': '2011',
-             'lopnummer': '1',
-             'is_published': '0',  # False (nothing is published yet)
-             'titel': u'Föreskrifter om ersättningssystem i kreditinstitut, värdepappersbolag och fondbolag med tillstånd för diskretionär portföljförvaltning',
-             'sammanfattning': u'Finansinspektionen har beslutat ... utan av FFFS 2011:2',
-             'forfattningssamling_id': '1',  # ID handled by import
-             'beslutsdatum': '2011-02-17',  # Use string not datetime
-             'ikrafttradandedatum': '2011-03-01',
-             'utkom_fran_tryck': '2011-02-24',
-             'omtryck': '0',  # Use '0' or '1' for boolean!
-             'content_md5': ''  # Should be calculated somewhere...
-             }
-            ],
-        'fs_doc_allmannarad':
-        [
-            {'fsdokument_ptr_id': '1',  # id is created when loading docs
-             'content': u'/allmannarad/fffs2011.pdf',
-             'beslutad_av_id': '',  # Always empty (we don't handle this yet)
-             'utgivare_id': ''  # Always empty (we don't handle this yet)
-             }
-            ],
-        'fs_doc_Myndighet':
-        [
-            {'id': '1',  # Default. Sometimes there is only one.
-             'namn': u'Finansinspektionen',
-             }
-            ],
-        'fs_doc_Forfattningssamling':
-        [
-            {'id': '1',  # Default. Sometimes there is only one.
-             'titel': u'Finansinspektionens författningssamling',
-             'kortnamn': u'FFFS',
-             'slug': u'fffs'
-             }
-        ]
-        }
+                [
+                    {'id': '1',  # id is created when loading docs
+                     'arsutgava': '2011',
+                     'lopnummer': '1',
+                     'is_published': '0',  # False (nothing is published yet)
+                     'titel': u'Föreskrifter om ersättningssystem i kreditinstitut, värdepappersbolag och fondbolag med tillstånd för diskretionär portföljförvaltning',
+                     'sammanfattning': u'Finansinspektionen har beslutat ... utan av FFFS 2011:2',
+                     'forfattningssamling_id': '1',  # ID handled by import
+                     'beslutsdatum': '2011-02-17',  # Use string not datetime
+                     'ikrafttradandedatum': '2011-03-01',
+                     'utkom_fran_tryck': '2011-02-24',
+                     'omtryck': '0',  # Use '0' or '1' for boolean!
+                     'content_md5': ''  # Should be calculated somewhere...
+                     },
+                    {'id': '2',  # id is created when loading docs
+                     'arsutgava': '2011',
+                     'lopnummer': '2',
+                     'is_published': '0',  # False (nothing is published yet)
+                     'titel': u'Föreskrifter om ändring i föreskrifter 2009:1 om administration hos statliga myndigheter',
+                     'sammanfattning': u'Finansinspektionen har beslutat ... utan av FFFS 2011:2',
+                     'forfattningssamling_id': '1',  # ID handled by import
+                     'beslutsdatum': '2011-04-01',  # Use string not datetime
+                     'ikrafttradandedatum': '2011-05-01',
+                     'utkom_fran_tryck': '2011-04-20',
+                     'omtryck': '0',  # Use '0' or '1' for boolean!
+                     'content_md5': ''  # Should be calculated somewhere...
+                     }
+                    ],
+                'fs_doc_allmannarad':
+                [
+                    {'fsdokument_ptr_id': '2',  # id is created when loading docs
+                     'content': u'/allmannarad/fs1102.pdf',
+                     'beslutad_av_id': '',  # Always empty (we don't handle this yet)
+                     'utgivare_id': ''  # Always empty (we don't handle this yet)
+                     }
+                    ],
+                'fs_doc_myndighetsforeskrift':
+                [
+                    {'fsdokument_ptr_id': '1',  # id is created when loading docs
+                     'content': u'/foreskrift/fs1101.pdf',
+                     'beslutad_av_id': '',  # Always empty (we don't handle this yet)
+                     'utgivare_id': ''  # Always empty (we don't handle this yet)
+                     }
+                    ],
+                'fs_doc_Myndighet':
+                [
+                    {'id': '1',  # Default. Sometimes there is only one.
+                     'namn': u'Finansinspektionen',
+                     }
+                    ],
+                'fs_doc_Forfattningssamling':
+                [
+                    {'id': '1',  # Default. Sometimes there is only one.
+                     'titel': u'Finansinspektionens författningssamling',
+                     'kortnamn': u'FFFS',
+                     'slug': u'fffs'
+                     }
+                ]
+                }
 
         fill_table('fs_doc_fsdokument', data, db_connection)
         fill_table('fs_doc_allmannarad', data, db_connection)
+        fill_table('fs_doc_myndighetsforeskrift', data, db_connection)
         fill_table('fs_doc_Myndighet', data, db_connection)
         fill_table('fs_doc_Forfattningssamling', data, db_connection)
 
