d44662f8b7f585027fd23be7f5ef3ad6a5fe7c2e
kamidev <jonas.beckman@gmail.com>
2012-02-10 15:59:13 +0100

    Add more logging functionality. Cloes #42.

--- fst_web/demo_settings.py
+++ fst_web/demo_settings.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
+
 ROOT = os.path.abspath(os.path.dirname(__file__))
 path = lambda *args: os.path.join(ROOT, *args)
 
-"""Default DEMO settings for FST webservice (fst_web)
+""" Template for local settings of the FST webservice (fst_web)
 
-Replace this file with a file named 'local_settings.py' for production use
+Please edit this file and replace all generic values with values suitable to
+your particular installation.
 """
 
-# NOTE! Always set this to FALSE before deploying
+# NOTE! Always set this to False before deploying
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -19,8 +21,28 @@ DATABASES = {
     }
 }
 
+LOG_LEVEL = "DEBUG"
+
+#Enable this to override global DB Debug setting
+#DB_DEBUG_LEVEL = "DEBUG"
+
+# Setup mail server for sending email notifications.
+# You can use any mail server you want.
+# But a very simple way to get started is to use a gmail account.
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'your email'
+#EMAIL_HOST_PASSWORD = 'your password'
+
+# Admins specified here receive email notifications on critical errors.
+# By default, this is the FST sysadmin at Domstolsverket.
+# Feel free to add more names if you want your local sysadmin to
+# receive the same notifications.
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+# ('Sysadmin at FST', 'sysadmin@fst.lagrummet.se',
+# ('Sysadmin at your agency', 'sysadmin@your_agency.se',
+#),
 )
 
 MANAGERS = ADMINS
@@ -31,7 +53,7 @@ FST_INSTANCE_PREFIX = "exfs"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = os.path.join("/" ,FST_INSTANCE_PREFIX, "dokument/")
+MEDIA_URL = os.path.join("/", FST_INSTANCE_PREFIX, "dokument/")
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '2i!!@xy4goq72irz5ldb%gmogc4w&#bgx64j6q9%l)r9^4i-v-'
@@ -57,3 +79,4 @@ FST_ORG_CONTACT_EMAIL = "lagrum@exempelmyndigheten.se"
 # These values will be supplied by Rättsinformationsprojektet.
 FST_DATASET_URI = "tag:exempelmyndigheten.se,2009:rinfo:feed"
 FST_DATASET_TITLE = u"Flöde för Exempelmyndighetens författningssamling"
+
