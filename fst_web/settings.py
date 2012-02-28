132a0452b8f44172b8b5d2003c26cbc26d3fccbe
kamidev <jonas.beckman@gmail.com>
2012-02-28 22:25:27 +0100

    Remove unused app 'admin_docs' from INSTALLED_APPS

--- fst_web/settings.py
+++ fst_web/settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    #'django.contrib.admindocs',
     # Application specific here
     'fst_web.fs_doc',
     'fst_web.adminplus'
