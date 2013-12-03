e31b050e3a856eb21a995747fc8b3a5a7f2d7d73
kamidev <jonas.beckman@gmail.com>
2013-12-03 14:02:36 +0100

    Add settings for another instance: fffs

---
+++ deploy/instance_settings/fffs/instance_settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Prefix of default document collection of this instance
+FST_INSTANCE_PREFIX = "fffs"
+
+# Organization authorized to publish these documents.
+FST_ORG_NAME = u"Finansinspektionen"
+FST_ORG_NAME_POSSESSIVE = u"Finansinspektionens"
+
+# Contact information for ATOM feed
+FST_ORG_CONTACT_NAME = u"Jonas Beckman"
+FST_ORG_CONTACT_URL = "http://www.fi.se/"
+FST_ORG_CONTACT_EMAIL = "lagrum.fffs@fi.se"
+
+# Description of data source for Atom feed.
+# These values will be supplied by Rättsinformationsprojektet.
+FST_DATASET_URI = "tag:finansinspektionen.se,2009:rinfo:feed"
+FST_DATASET_TITLE = u"Flöde för Finansinspektionens författningssamling"
+
+# Make the secret key below unique, and don't share it with anybody.
+
