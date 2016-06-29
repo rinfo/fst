f1c9b1f6216a8001395c638a44857d498b309a84
kamidev <jonas.beckman@gmail.com>
2016-06-29 09:44:37 +0200

    Fix Python 3 error

--- fst_web/fs_doc/models.py
+++ fst_web/fs_doc/models.py
@@ -799,7 +799,7 @@ def to_slug(tag):
     As specified by:
     http://dev.lagrummet.se/dokumentation/system/uri-principer.pdf
     """
-    tag = tag.lower().encode("utf-8")
+    tag = tag.lower()
     slug = tag.replace('å', 'aa').replace('ä', 'ae').\
          replace('ö', 'oe').replace(' ', '_')
     return slug
