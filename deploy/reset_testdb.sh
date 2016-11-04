293673db477cf373eb6fc0bd0841ad5ea88ee599
kamidev <jonas.beckman@gmail.com>
2016-11-04 13:57:24 +0100

    Update demo DB script

--- deploy/reset_testdb.sh
+++ deploy/reset_testdb.sh
@@ -1,7 +1,8 @@
 mv fst_web/database/fst_demo.db db_bak.db
 python manage.py makemigrations
 python manage.py migrate
- python manage.py loaddata --settings=fst_web.settings fst_web/fs_doc/fixtures/exempeldata.json
+python manage.py loaddata --settings=fst_web.settings fst_web/fs_doc/fixtures/default_users.json
+ python manage.py loaddata --settings=fst_web.settings fst_web/fs_doc/fixtures/demo_documents.json
 cp fst_web/fs_doc/fixtures/allmanna_rad/*.pdf fst_web/uploads
 cp fst_web/fs_doc/fixtures/foreskrift/*.pdf fst_web/uploads
 cp fst_web/fs_doc/fixtures/bilaga/*.pdf fst_web/uploads
