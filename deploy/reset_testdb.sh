658c761817d831707e28055671cb7538cc63176e
kamidev <jonas.beckman@gmail.com>
2016-11-06 20:39:31 +0100

    Add test run to database script

--- deploy/reset_testdb.sh
+++ deploy/reset_testdb.sh
@@ -9,3 +9,4 @@ cp fst_web/fs_doc/fixtures/foreskrift/*.pdf fst_web/uploads/foreskrift/
 cp fst_web/fs_doc/fixtures/bilaga/*.pdf fst_web/uploads/ovrigt/
 cp fst_web/fs_doc/fixtures/konsoliderad_foreskrift/*.pdf fst_web/uploads/konsoliderad_foreskrift/
 cp fst_web/fs_doc/fixtures/demo-database/fst_demo.db fst_web/database/fst_demo.db
+python manage.py test
