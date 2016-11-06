85fe55402ca1070c614e7133b3a7ee4e6a012cec
kamidev <jonas.beckman@gmail.com>
2016-11-06 18:46:05 +0100

    Add demo database to fixtures

--- deploy/reset_testdb.sh
+++ deploy/reset_testdb.sh
@@ -1,10 +1,11 @@
-rm fst_web/database/fst_demo.db
-python manage.py makemigrations fs_doc
-python manage.py migrate
-python manage.py loaddata --settings=fst_web.settings fst_web/fs_doc/fixtures/default_users.json
- python manage.py loaddata --settings=fst_web.settings fst_web/fs_doc/fixtures/demo_documents.json
-cp fst_web/fs_doc/fixtures/allmanna_rad/*.pdf fst_web/uploads
-cp fst_web/fs_doc/fixtures/foreskrift/*.pdf fst_web/uploads
-cp fst_web/fs_doc/fixtures/bilaga/*.pdf fst_web/uploads
-cp fst_web/fs_doc/fixtures/konsoliderad_foreskrift/*.pdf fst_web/uploads
-python manage.py test
+#!/usr/bin/env bash
+rm -rf fst_web/uploads/*
+mkdir fst_web/uploads/allmanna_rad/
+mkdir fst_web/uploads/foreskrift/
+mkdir fst_web/uploads/ovrigt/
+mkdir fst_web/uploads/konsoliderad_foreskrift/
+cp fst_web/fs_doc/fixtures/allmanna_rad/*.pdf fst_web/uploads/allmanna_rad/
+cp fst_web/fs_doc/fixtures/foreskrift/*.pdf fst_web/uploads/foreskrift/
+cp fst_web/fs_doc/fixtures/bilaga/*.pdf fst_web/uploads/ovrigt/
+cp fst_web/fs_doc/fixtures/konsoliderad_foreskrift/*.pdf fst_web/uploads/konsoliderad_foreskrift/
+cp fst_web/fs_doc/fixtures/demo-database/fst_demo.db fst_web/database/fst_demo.db
