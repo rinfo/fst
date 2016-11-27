ec7a29090109259e3c5b6682ad09d6a097109567
kamidev <jonas.beckman@gmail.com>
2016-11-27 22:25:17 +0100

    Rename DB restore script

--- deploy/restore_demo_db.sh
+++ deploy/restore_demo_db.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 rm -rf fst_web/uploads/*
 mkdir fst_web/uploads/allmanna_rad/
 mkdir fst_web/uploads/foreskrift/
@@ -10,3 +11,5 @@ cp fst_web/fs_doc/fixtures/bilaga/*.pdf fst_web/uploads/ovrigt/
 cp fst_web/fs_doc/fixtures/konsoliderad_foreskrift/*.pdf fst_web/uploads/konsoliderad_foreskrift/
 cp fst_web/fs_doc/fixtures/demo-database/fst_demo.db fst_web/database/fst_demo.db
 python manage.py test
+echo
+echo "Sucessfully restored FST demo database!"
